fee048d4f6f009f0ea002f0ecb08cd53fd3d7bed
Сергей Буркун <GizmoG2@live.ru>
2023-10-21 21:48:14 +0300

    Создан проэкт, основные обьекты, скрипты.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] int ForcePover;
+    public float Force; // Add force amount
+    public Rigidbody2D PlayerObject; // Player Object
+
+    private void Update()
+    {
+        // Check keyboard keys
+        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            AddForceToPlayer(new Vector2(0, Force * ForcePover)); // Move to up
+        }
+        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            AddForceToPlayer(new Vector2(-Force, 0)); // Move to left
+        }
+        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            AddForceToPlayer(new Vector2(Force, 0)); // Move to right
+        }
+    }
+    
+    private void AddForceToPlayer(Vector2 force)
+    {
+        PlayerObject.AddForce(force); // Add force to player
+    }
+}
